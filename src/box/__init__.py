b480bad34bac4ace766a548e9dc76a2f39d92643
Vincent <vincentbusiness55@gmail.com>
2021-12-14 02:30:04 -0800

    :ambulance:

--- src/box/__init__.py
+++ src/box/__init__.py
@@ -7,4 +7,25 @@ Pincer extension adding extra features.
 :license: GNU Affero
 """
 
+__title__ = 'box'
+__author__ = 'VincentRPS'
+__license__ = 'GNU Affero'
+__copyright__ = 'Copyright 2021 VincentRPS'
+__version__ = '0.1'
+
+import logging
+from typing import NamedTuple, Literal
+
 from . import *
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=0, releaselevel='alpha', serial=0)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())

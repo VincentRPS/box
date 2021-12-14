720e3b56670162859df466995cb2161c607db965
Vincent <vincentbusiness55@gmail.com>
2021-12-14 02:52:15 -0800

    Make Version Info Use Years Instead Of Majors

--- src/box/__init__.py
+++ src/box/__init__.py
@@ -11,21 +11,11 @@ __title__ = 'box'
 __author__ = 'VincentRPS'
 __license__ = 'GNU Affero'
 __copyright__ = 'Copyright 2021 VincentRPS'
-__version__ = '0.1.0'
+__version__ = '2021.x.x' # TODO: Specify release at some point
 
 import logging
 from typing import NamedTuple, Literal
 
 from . import *
-
-class VersionInfo(NamedTuple):
-    major: int
-    minor: int
-    micro: int
-    releaselevel: Literal["alpha", "beta", "candidate", "final"]
-    serial: int
-
-
-version_info: VersionInfo = VersionInfo(major=0, minor=1, micro=0, releaselevel='alpha', serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
